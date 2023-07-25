61310019df1a4a8ae582703e7952ad3ed0907d19
F1ko228 <nik-shestakov-03@mail.ru>
2023-07-26 02:42:08 +0700

    one btn modalWindow

---
+++ assets/main.js
@@ -0,0 +1,4 @@
+document.getElementById("section_6__ModalWindow_btn").addEventListener('click', function() {
+    let value = document.querySelector("#section_6__modalWindow");
+    value.classList.add('open');
+})
